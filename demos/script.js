81c57859eca1ea5e973e1169853ba830781bb42a
Gabrielle von Koss <gabivk@gmail.com>
2016-11-29 13:50:15 +0000

    component WIP

--- demos/script.js
+++ demos/script.js
@@ -2,10 +2,9 @@ Promise.all([
 	fetch('../templates/main.html')
 		.then(res => res.text()),
 	fetch('./dummy-data.json')
-		.then(res => res.json())
+		.then(res => res.json()),
 ])
 	.then(([source, data]) => {
-		console.log('OI', data);
 		const template = Handlebars.compile(source);
-		document.querySelector('body').innerHTML = template(data);
+		document.querySelector('.supa-test').innerHTML = template(data);
 	});

7d5494c33ec5f841fc34b968d9d9f281af47e6bd
TheoLeanse <theoleanse@gmail.com>
2016-11-30 17:43:03 +0000

    add temporary opacity datamunger

--- demos/script.js
+++ demos/script.js
@@ -5,14 +5,26 @@ Promise.all([
 		.then(res => res.json()),
 ])
 	.then(([source, data]) => {
+		const yearTotals = data.children.map(x => x.total).sort((a, b) => a - b);
+		const yearMax = yearTotals[yearTotals.length - 1];
+
+		const monthTotals = data.children[1].children.map(x => x.total).sort((a, b) => a - b);
+		const monthMax = monthTotals[monthTotals.length - 1];
+
+		const addOpacity = (x, max) => Object.assign(x, { opacity: x.total / max });
+
+		data.children.forEach((child, i) => {
+			data.children = data.children.map(year => addOpacity(year, yearMax));
+			child.children = child.children.map(month => addOpacity(month, monthMax));
+		});
+
+		return [source, data];
+	})
+	.then(([source, data]) => {
 		const template = Handlebars.compile(source);
 
 		Handlebars.registerHelper('abbreviate', (word) => {
 			return typeof word === 'string' ? word.substring(0,3) : word;
-		})
-
-		Handlebars.registerHelper('opacity', (total) => {
-			return total / 75;
 		})
 
 		Handlebars.registerHelper('timestamp', (day, month, year) => {
--- main.js
+++ main.js
@@ -3,25 +3,38 @@ Promise.all([fetch('../templates/main.html')
 fetch('./vwdata.json')
 	.then(res => res.json())
 ])
+	.then(([source, data]) => {
+		const yearTotals = data.children.map(x => x.total).sort((a, b) => a - b);
+		const yearMax = yearTotals[yearTotals.length - 1];
+
+		const monthTotals = data.children[1].children.map(x => x.total).sort((a, b) => a - b);
+		const monthMax = monthTotals[monthTotals.length - 1];
+
+		const addOpacity = (x, max) => Object.assign(x, { opacity: x.total / max });
+
+		data.children.forEach((child, i) => {
+			data.children = data.children.map(year => addOpacity(year, yearMax));
+			child.children = child.children.map(month => addOpacity(month, monthMax));
+		});
+
+		return [source, data];
+	})
 	.then(([source, initialData]) => {
 		const template = Handlebars.compile(source);
 		const segments = document.getElementsByClassName('heatmap-segment');
-		const back = document.getElementsByClassName('back')
+		const back = document.getElementsByClassName('back');
 
-		console.log(back);
 		addClickEvent(initialData);
 
 		function addClickEvent (data) {
-			// const back = document.querySelector('.back');
 			back[0].style.display = data === initialData ? 'none' : '';
 
 			for (var i = 0; i < segments.length; i++) {
 				if (!data.children) return;
-
-				let childData = data.children[i]
+				let childData = data.children[i];
 				segments[i].addEventListener('click', () => {
-					expandSegments(childData)
-					setBackButton(data)
+					expandSegments(childData);
+					setBackButton(data);
 				});
 			}
 		}
@@ -32,11 +45,9 @@ fetch('./vwdata.json')
 		}
 
 		function setBackButton (data) {
-			// const back = document.querySelector('.back');
-
 			back[0].addEventListener('click', () => {
 				expandSegments(data);
-				data === initialData ? setBackButton(data) : setBackButton(initialData)
-			})
+				data === initialData ? setBackButton(data) : setBackButton(initialData);
+			});
 		}
-	})
+	});

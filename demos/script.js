068da6f998cb0c989c45bfd4ca744ade082332d9
Gabrielle von Koss <gabivk@gmail.com>
2016-11-30 15:11:14 +0000

    added some sweet helpers

--- demos/script.js
+++ demos/script.js
@@ -7,11 +7,19 @@ Promise.all([
 	.then(([source, data]) => {
 		const template = Handlebars.compile(source);
 
+		Handlebars.registerHelper('abbreviate', (word) => {
+			return typeof word === 'string' ? word.substring(0,3) : word;
+		})
+
+		Handlebars.registerHelper('opacity', (total) => {
+			return total / 10;
+		})
+
 		const oneYear = data.children[2];
 		const oneMonth = oneYear.children[11];
 		document.querySelector('.container').innerHTML = template(data);
 
 		const script = document.createElement('script');
 		script.src = '../main.js';
-		document.head.appendChild(script);		
+		document.head.appendChild(script);
 });

4b16cac185edaf20f4de146255b7b222d9a766e5
Gabrielle von Koss <gabivk@gmail.com>
2016-12-01 16:46:01 +0000

    slimming helper

--- demos/script.js
+++ demos/script.js
@@ -32,13 +32,6 @@ Promise.all([
 			return date.toLocaleString('en-US', { day: "numeric", month: "long", year: "numeric" });
 		});
 
-		Handlebars.registerHelper('link', (article) => {
-			let url = Handlebars.escapeExpression(`https://www.ft.com/content/${article.id}`);
-			let title = Handlebars.escapeExpression(article.title)
-
-			return new Handlebars.SafeString(`<a href= ${url}>${title}</a>`)
-		})
-
 		document.body.innerHTML = template(data);
 
 		const script = document.createElement('script');

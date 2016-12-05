12f46da10d43a5d1077e59768bfa878f271bbceb
Gabrielle von Koss <gabivk@gmail.com>
2016-12-05 13:28:22 +0000

    much change, so handlebars

--- demos/script.js
+++ demos/script.js
@@ -1,24 +1,28 @@
-Promise.all([
-	fetch('../templates/main.html')
-		.then(res => res.text()),
-	fetch('./N2FkZjRhMWUtZDZjNS00ZTQ0LTg1MTMtMjYyYzBlODkzYTQ2-UE4=.json')
-		.then(res => res.json()),
-])
-	.then(([source, data]) => {
-		const template = Handlebars.compile(source);
-
-		Handlebars.registerHelper('abbreviate', (word) => {
-			return typeof word === 'string' ? word.substring(0,3) : word;
-		});
-
-		Handlebars.registerHelper('timestamp', (day, month, year) => {
-			let date = new Date(year, month, day);
-			return date.toLocaleString('en-US', { day: "numeric", month: "long", year: "numeric" });
-		});
-
-		document.body.innerHTML = template(data);
-
-		const script = document.createElement('script');
-		script.src = '../main.js';
-		document.head.appendChild(script);
-});
+// Promise.all([
+// 	fetch('../templates/main.html')
+// 		.then(res => res.text()),
+// 	fetch('./N2FkZjRhMWUtZDZjNS00ZTQ0LTg1MTMtMjYyYzBlODkzYTQ2-UE4=.json')
+// 		.then(res => res.json()),
+// // ])
+// // 	.then(([source, data]) => {
+// 	const Handlebars = require('handlebars')
+// 	// const source = require('./N2FkZjRhMWUtZDZjNS00ZTQ0LTg1MTMtMjYyYzBlODkzYTQ2-UE4=.json');
+// 	// const initialData = require('../templates/main.html');
+// 	//
+// 	// 	const template = Handlebars.compile(source);
+//
+// 		Handlebars.registerHelper('abbreviate', (word) => {
+// 			return typeof word === 'string' ? word.substring(0,3) : word;
+// 		});
+//
+// 		Handlebars.registerHelper('timestamp', (day, month, year) => {
+// 			let date = new Date(year, month, day);
+// 			return date.toLocaleString('en-US', { day: "numeric", month: "long", year: "numeric" });
+// 		});
+//
+// 		// document.body.innerHTML = template(data);
+// 		//
+// 		// const script = document.createElement('script');
+// 		// script.src = '../main.js';
+// 		// document.head.appendChild(script);
+// // });

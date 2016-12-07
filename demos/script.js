2f166f46e9d8521bed9b020ed8c3989c12125283
Gabrielle von Koss <gabivk@gmail.com>
2016-12-07 15:39:38 +0000

    got dots in place!!! also, merge conflict inducing partials.

--- demos/script.js
+++ demos/script.js
@@ -16,7 +16,54 @@ Promise.all([
 			return date.toLocaleString('en-US', { day: "numeric", month: "long", year: "numeric" });
 		});
 
-		document.body.innerHTML = template(data);
+		fetch('../templates/partials/months.html')
+		.then(res => res.text())
+		.then(monthsPartial => {
+			Handlebars.registerPartial('months', monthsPartial)
+		})
+
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+		const dotData = yearDots(data);
+
+		function yearDots (data) {
+			data.children.forEach(year => {
+				year['dot'] = []
+			})
+
+			for(let y = 0; y < data.relevantArticles.length; y++){
+				let article = data.relevantArticles[y]
+				for(let i = 0; i < data.children.length; i++) {
+					let year = data.children[i];
+					if( year.name === article.year) {
+						year['dot'].push({name: y+1, position: Math.ceil((100 / 12) * article.month)})
+					}
+					monthDots(year)
+				}
+			}
+			return data;
+		}
+
+
+		function monthDots (data) {
+			data.children.forEach(month => {
+				month['dot'] = []
+			})
+
+			for(let y = 0; y < data.relevantArticles.length; y++){
+				let article = data.relevantArticles[y]
+				for(let i = 0; i < data.children.length; i++) {
+					let month = data.children[i];
+					if( month.name === months[article.month]) {
+						month['dot'].push({name: y+1, position: Math.ceil((100 / 30) * article.day)})
+					}
+				}
+			}
+		}
+
+
+
+		document.body.innerHTML = template(dotData);
 
 		const script = document.createElement('script');
 		script.src = '../main.js';
--- main.js
+++ main.js
@@ -8,9 +8,49 @@ Promise.all([
 		const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment-colour');
 		const backBtns = document.getElementsByClassName('n-storylines__back-btn');
 
-		setupInteraction(initialData);
+		const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+		const dotData = yearDots(initialData);
+		
+		function yearDots (data) {
+			data.children.forEach(year => {
+				year['dot'] = []
+			})
+
+			for(let y = 0; y < data.relevantArticles.length; y++){
+				let article = data.relevantArticles[y]
+				for(let i = 0; i < data.children.length; i++) {
+					let year = data.children[i];
+					if( year.name === article.year) {
+						year['dot'].push({name: y+1, position: Math.ceil((100 / 12) * article.month)})
+					}
+					monthDots(year)
+				}
+			}
+			return data;
+		}
+
+
+		function monthDots (data) {
+			data.children.forEach(month => {
+				month['dot'] = []
+			})
+
+			for(let y = 0; y < data.relevantArticles.length; y++){
+				let article = data.relevantArticles[y]
+				for(let i = 0; i < data.children.length; i++) {
+					let month = data.children[i];
+					if( month.name === months[article.month]) {
+						month['dot'].push({name: y+1, position: Math.ceil((100 / 30) * article.day)})
+					}
+				}
+			}
+		}
+
+		setupInteraction(dotData);
 
 		function setupInteraction (data) {
+			console.log(data);
 			backBtns[0].style.display = data === initialData ? 'none' : '';
 			if (!data.children) return;
 
@@ -30,7 +70,7 @@ Promise.all([
 		function setupBackBtn (data) {
 			backBtns[0].addEventListener('click', () => {
 				renderStoryline(data);
-				setupBackBtn(initialData);
+				setupBackBtn(dotData);
 			});
 		}
 	});

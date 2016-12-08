5ebf1e60632a70e9355ac72ddfbf31ef200f4f52
TheoLeanse <theoleanse@gmail.com>
2016-12-08 15:09:00 +0000

    linting

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 const template = require('./templates/main.html');
-const yearDots = require('./lib/dotPosition')
+const yearDots = require('./lib/dotPosition');
 
 function init(initialData) {
-    const component = document.querySelector('.n-storylines');
-    const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment-colour');
-    const backBtns = document.getElementsByClassName('n-storylines__back-btn');
-    const dotData = yearDots(initialData);
-		
-    setupInteraction(dotData);
+	const component = document.querySelector('.n-storylines');
+	const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment-colour');
+	const backBtns = document.getElementsByClassName('n-storylines__back-btn');
+	const dotData = yearDots(initialData);
 
-    function setupInteraction(data) {
-        backBtns[0].style.display = data === initialData ? 'none' : '';
-        if (!data.children) return;
+	setupInteraction(dotData);
 
-        for (let i = 0; i < heatmapSegments.length; i++) {
-            if (data.children[i].relevantArticles.length > 0) {
-                heatmapSegments[i].addEventListener('click', () => {
-                    renderStoryline(data.children[i]);
-                    setupBackBtn(data);
-                });
-            } else {
-                let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name')
-                heatmapSegmentsNames[i].classList.add('no-articles')
-            }
-        }
-    }
+	function setupInteraction(data) {
+		backBtns[0].style.display = data === initialData ? 'none' : '';
+		if (!data.children) return;
 
-    function renderStoryline(data) {
-        component.innerHTML = template(data);
-        setupInteraction(data);
-    }
+		for (let i = 0; i < heatmapSegments.length; i++) {
+			if (data.children[i].relevantArticles.length > 0) {
+				heatmapSegments[i].addEventListener('click', () => {
+					renderStoryline(data.children[i]);
+					setupBackBtn(data);
+				});
+			} else {
+				let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name');
+				heatmapSegmentsNames[i].classList.add('no-articles');
+			}
+		}
+	}
 
-    function setupBackBtn(data) {
-        backBtns[0].addEventListener('click', () => {
-            renderStoryline(data);
-            setupBackBtn(dotData);
-        });
-    }
+	function renderStoryline(data) {
+		component.innerHTML = template(data);
+		setupInteraction(data);
+	}
+
+	function setupBackBtn(data) {
+		backBtns[0].addEventListener('click', () => {
+			renderStoryline(data);
+			setupBackBtn(dotData);
+		});
+	}
 }
 
 module.exports = {
-    init
+	init
 };

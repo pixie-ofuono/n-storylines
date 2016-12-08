67fcf7f97d9238da477f9cccb8550307bfab83c5
Gabrielle von Koss <gabivk@gmail.com>
2016-12-08 15:04:42 +0000

    tabness?

--- main.js
+++ main.js
@@ -2,43 +2,43 @@ const template = require('./templates/main.html');
 const yearDots = require('./lib/dotPosition')
 
 function init(initialData) {
-  const component = document.querySelector('.n-storylines');
-  const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment-colour');
-  const backBtns = document.getElementsByClassName('n-storylines__back-btn');
-  const dotData = yearDots(initialData);
+    const component = document.querySelector('.n-storylines');
+    const heatmapSegments = document.getElementsByClassName('n-storylines__heatmap-segment-colour');
+    const backBtns = document.getElementsByClassName('n-storylines__back-btn');
+    const dotData = yearDots(initialData);
+		
+    setupInteraction(dotData);
 
-  setupInteraction(dotData);
+    function setupInteraction(data) {
+        backBtns[0].style.display = data === initialData ? 'none' : '';
+        if (!data.children) return;
 
-  function setupInteraction(data) {
-    backBtns[0].style.display = data === initialData ? 'none' : '';
-    if (!data.children) return;
-
-    for (let i = 0; i < heatmapSegments.length; i++) {
-      if (data.children[i].relevantArticles.length > 0) {
-        heatmapSegments[i].addEventListener('click', () => {
-            renderStoryline(data.children[i]);
-            setupBackBtn(data);
-        });
-    } else {
-        let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name')
-        heatmapSegmentsNames[i].classList.add('no-articles')
-      }
+        for (let i = 0; i < heatmapSegments.length; i++) {
+            if (data.children[i].relevantArticles.length > 0) {
+                heatmapSegments[i].addEventListener('click', () => {
+                    renderStoryline(data.children[i]);
+                    setupBackBtn(data);
+                });
+            } else {
+                let heatmapSegmentsNames = document.querySelectorAll('.n-storylines__heatmap-segment-name')
+                heatmapSegmentsNames[i].classList.add('no-articles')
+            }
+        }
     }
-  }
 
-  function renderStoryline(data) {
-    component.innerHTML = template(data);
-    setupInteraction(data);
-  }
+    function renderStoryline(data) {
+        component.innerHTML = template(data);
+        setupInteraction(data);
+    }
 
-  function setupBackBtn(data) {
-    backBtns[0].addEventListener('click', () => {
-      renderStoryline(data);
-      setupBackBtn(dotData);
-    });
-  }
+    function setupBackBtn(data) {
+        backBtns[0].addEventListener('click', () => {
+            renderStoryline(data);
+            setupBackBtn(dotData);
+        });
+    }
 }
 
 module.exports = {
-  init
+    init
 };
